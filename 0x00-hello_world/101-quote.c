2c31bf2d7d301f6c77d64f47ab16855e9a144749
Ivosin1 <ivshonowo@gmail.com>
2023-08-11 15:01:47 +0100

    The 101-quote task.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Programme that writes a quote without using printf and putf
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+}
