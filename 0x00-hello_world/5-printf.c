bd5364d38e6a78a3e3c1871d663c6dccfe22e591
Ivosin1 <ivshonowo@gmail.com>
2023-08-11 04:57:01 +0100

    C program that makes use of 'printf' command instead of 'puts' and goes to a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints a sentence followed by a new line
+ *
+ * Using the 'printf' command instead of 'puts' this time around.
+ * Return: Always 0 (Succeful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
