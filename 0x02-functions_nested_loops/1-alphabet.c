e468c0e2611dc2beb43679cd13bafc057f7661b6
Ivosin1 <ivshonowo@gmail.com>
2023-08-16 05:26:05 +0100

    A programme that prints the alphabet.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alp_;
+
+	for (alp_ = 'a'; alp_ <= 'z'; alp_++)
+	_putchar (alp_);
+
+	_putchar ('\n');
+}
