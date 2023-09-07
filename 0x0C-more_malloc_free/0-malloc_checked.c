f1e77cf3157b905006832b8dcd336c06b33337da
Ivosin1 <ivshonowo@gmail.com>
2023-09-07 12:48:13 +0100

    More prereq files.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * *malloc_checked -  function that allocates memory using malloc
+ * @b: size of the memory block to be allocated
+ * Return: pointer to the allocated memory
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	void *block;
+
+	block = malloc(b);
+	if (block == NULL)
+		exit(98);
+
+	return (block);
+}
