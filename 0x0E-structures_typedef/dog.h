ee740bdd98ea0d724dff92714cc5806727ea25c8
Ivosin1 <ivshonowo@gmail.com>
2023-09-12 05:12:54 +0100

    A structure definition for the description of a dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type of data describing a dog.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
