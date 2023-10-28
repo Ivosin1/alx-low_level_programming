74cb9556335e5107376a4e75fdc14efa50b99876
Ivosin1 <ivshonowo@gmail.com>
2023-10-28 11:26:54 +0100

    Description adjusted.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Write a C program that prints a sentence followed by a new line
+ * using the 'printf' command instead of 'puts' this time around.
  *
- * Using the 'printf' command instead of 'puts' this time around.
  * Return: Always 0 (Succeful)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
