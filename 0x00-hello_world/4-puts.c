8bbda257a03146f1e77b753e1a03213caa4c1f5e
Ivosin1 <ivshonowo@gmail.com>
2023-10-28 09:00:08 +0100

    Description updated.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Write a C program that prints a sentence followed by a new line
+ * using the puts command instead of printf.
  *
- * Using the puts command instead of printf
- * Return: Always 0 (Succeful)
+ * Return: Always 0 (Succeful).
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
