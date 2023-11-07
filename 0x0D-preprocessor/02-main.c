241a005052adb23e4b175116a904f5c103792edd
Ivosin1 <ivshonowo@gmail.com>
2023-11-07 05:59:07 +0100

    My revised 0x0D-preprocessor task.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print out file name of source code.
+ *
+ * Return: Always 0 (To ascertain a successful run).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
